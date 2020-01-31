b75c56ab1d6d41cc030f694eaa83f8c790f4a977
Harry Urban <hurban@stroeer.de>
2020-01-31 12:44:25 +0100

    use tf cloud instead of s3 backend

--- state_s3bucket.tf
+++ state_s3bucket.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  profile    = "arc-demo"
-  region     = "eu-west-1"
-}
-
-resource "aws_s3_bucket" "b" {
-  bucket = "terraform-remote-state-workspace-harry-test"
-  acl    = "private"
-
-  tags = {
-    Name        = "Terraform Workspace Remote State"
-    managed_by = "terraform",
-    team = "Buzz"
-  }
-}
+//provider "aws" {
+//  profile    = "arc-demo"
+//  region     = "eu-west-1"
+//}
+//
+//resource "aws_s3_bucket" "b" {
+//  bucket = "terraform-remote-state-workspace-harry-test"
+//  acl    = "private"
+//
+//  tags = {
+//    Name        = "Terraform Workspace Remote State"
+//    managed_by = "terraform",
+//    team = "Buzz"
+//  }
+//}
