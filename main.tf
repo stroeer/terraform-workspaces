cb734777ab23afc579c5e56fc8536b72542fa594
Harry Urban <hurban@stroeer.de>
2020-01-31 12:44:46 +0100

    move main.tf to root folder

--- main.tf
+++ main.tf
@@ -3,16 +3,17 @@ provider "aws" {
   region     = "eu-west-1"
 }
 
-// Using S3 as a remote state backend does not support states per workspace
 
-//terraform {
-//  backend "s3" {
-//    bucket = "terraform-remote-state-workspace-harry-test"
-//    key    = "terraform/remotestate"
-//    region = "eu-west-1"
-//  }
-//
-//}
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "stroeer"
+
+    workspaces {
+      prefix = "ton-"
+    }
+  }
+}
 
 module "variables" {
   source = "github.com/stroeer/terraform-playground-variables"
